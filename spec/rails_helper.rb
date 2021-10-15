c63b2ce339dcd3b0f84c782ccd0917c67e130856
kosjan199 <akishe14@gmail.com>
2021-10-15 15:41:55 +0300

    Fix bug with rspec work

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,8 +32,8 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
-  #config.include Devise::Test::ControllerHelpers
   config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include Devise::Test::ControllerHelpers, type: :controller
   config.extend ControllerMacros, type: :controller
   config.include AcceptanceMacros, type: :feature
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
